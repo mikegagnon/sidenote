25423dbe20d410c64debca354791e896b16f4e24
Michael Gagnon <mikegagnon@gmail.com>
2017-11-16 15:16:16 -0700

    Seems to work

--- obscure.py
+++ obscure.py
@@ -4,10 +4,39 @@
 #
 
 import argparse
+import random
+import re
+import string
+
+# https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python
+key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+
+SIDENOTE_LINK_PARSER = re.compile(r'\[(?P<linktext>[^\]]*)\]\(##(?P<identifier>[^)]*)\)')
+LINK_PARSER = re.compile(r'(\[[^\]]*\]\([^)]*\))')
+TILDE_ANCHOR_PARSER = re.compile(r'^~.*$')
 
 
 def obscure(filename):
-    pass
+    with open(filename) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        newline = ""
+
+        # tokenize the line into links and non-links
+        for part in LINK_PARSER.split(line):
+            if LINK_PARSER.match(part):
+                newpart = part.replace("(##", "(##" + key)
+                newline += newpart
+            else:
+                newline += part
+
+        if TILDE_ANCHOR_PARSER.match(newline):
+            print "FOO"
+            newline = newline.replace("~", "~" + key)
+
+        print newline,
+
 
 if __name__=="__main__":
   parser = argparse.ArgumentParser(description='"Obscure" links in a Sidenote document')
